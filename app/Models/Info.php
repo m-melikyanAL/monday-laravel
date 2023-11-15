a26d52e451f99f8e3276142da9521be949112fcc
m-melikyanAL <meri.melikian100@gmail.com>
2023-11-15 14:32:00 +0400

    bug fixes

--- app/Models/Info.php
+++ app/Models/Info.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Info extends Model
 {
-    protected $table = 'items';
+    protected $table = 'info';
 
     protected $fillable = ['creatorId', 'name', 'created_at', 'updated_at', 'mondayId','creatorName'];
     public $timestamps = false;
